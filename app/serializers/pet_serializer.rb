f947bcfad92c90ab24771d05e5a483a8d2526662
github username <clarencekwong7@gmail.com>
2019-07-10 19:14:10 -0400

    Added service url

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,6 +1,12 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :breed, :instagram, :missing
+  include Rails.application.routes.url_helpers
+  attributes :id, :name, :breed, :instagram, :missing, :imageUrl
   belongs_to :user
   has_many :posters
   has_many :tags
+
+  def imageUrl
+    self.object.photo.service_url
+  end
 end
+p
