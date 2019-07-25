52dc7ea572787b642ac0413b493e970a38065e42
github username <clarencekwong7@gmail.com>
2019-07-25 15:10:27 -0400

    updated poster serializer

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -1,4 +1,4 @@
 class PosterSerializer < ActiveModel::Serializer
-  attributes :lat, :long, :pet_description, :poster_name, :poster_phone
+  attributes :lat, :long, :pet_description, :poster_name, :poster_phone, :found_lat, :found_long
   belongs_to :pet
 end
