428ef22344f4ea7844303245e6229812e5f37ddc
github username <clarencekwong7@gmail.com>
2019-09-23 15:16:48 -0400

    updated poster serializer to include reward

--- app/serializers/poster_serializer.rb
+++ app/serializers/poster_serializer.rb
@@ -1,5 +1,5 @@
 class PosterSerializer < ActiveModel::Serializer
-  attributes :id, :lat, :long, :pet_description, :poster_name, :poster_phone, :found_lat, :found_long, :missing_time, :comment, :additional_info
+  attributes :id, :lat, :long, :pet_description, :poster_name, :poster_phone, :found_lat, :found_long, :missing_time, :comment, :additional_info, :reward
   belongs_to :pet
   has_many :messages
 end
