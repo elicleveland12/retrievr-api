4b65b7766fa29484fafdcb40d742680038a9cdd0
github username <clarencekwong7@gmail.com>
2019-11-23 20:16:34 -0500

    shelter and breeder optional

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,8 @@
 class Pet < ApplicationRecord
     has_one_attached :image
-    belongs_to :user
-    belongs_to :litter
-    belongs_to :shelter
+    belongs_to :user, optional: true
+    belongs_to :litter, optional: true
+    belongs_to :shelter, optional: true
     has_many :posters
     has_many :tags
 end
