24bffc374322992f746134c16a38f4807acf2e5a
elicleveland12 <elicleveland12@gmail.com>
2023-06-10 08:06:52 -0600

    removed litter and shelter association from pets model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,8 +1,6 @@
 class Pet < ApplicationRecord
     has_one_attached :image
     belongs_to :user, optional: true
-    belongs_to :litter, optional: true
-    belongs_to :shelter, optional: true
     has_many :posters
     has_many :tags
     has_many :park_visits
