ffe9cd1144c1f5276dfcc0fd53e5e41708362a21
elicleveland12 <elicleveland12@gmail.com>
2023-07-01 11:06:37 -0600

    removed test cron function

--- app/models/park_visit.rb
+++ app/models/park_visit.rb
@@ -39,31 +39,4 @@ class ParkVisit < ApplicationRecord
     end
   end
 
-  def self.test_notify_owners
-    title = "Still Playing?"
-    body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
-    notifData = {
-      notificationType: "PARK_TIMEOUT",
-      parkVisitId: 1,
-      petId: 2
-    }
-
-    uri = URI.parse('https://exp.host/--/api/v2/push/send')
-
-    request = Net::HTTP::Post.new(uri)
-    request['Accept'] = 'application/json'
-    request['Content-Type'] = 'application/json'
-    
-    request.body = {
-      to: "ExponentPushToken[7Njq_KCVUhYLxc0U7DR8p-]",
-      title: title,
-      body: body,
-      data: notifData
-    }.to_json
-
-    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
-      http.request(request)
-    end
-  end
-
 end
